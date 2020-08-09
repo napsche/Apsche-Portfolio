c64cd240110e53a76e322d56f2e6ae3f0ecd70c4
Nicole Apsche <napsche@kgtiger.com>
2020-08-09 17:37:28 -0400

    editing intro modal

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
     $("#masthead").hide();
     $("#main-container").hide();
+
+    $("#entermodal").on("click", function() {
+        event.preventDefault();
+
+        $("#enter").remove();
+        $("#intro").remove();
+        $("#intro2").remove();
+
+        $("#main-container").show();
+        $("#masthead").show();
+    })
 })
